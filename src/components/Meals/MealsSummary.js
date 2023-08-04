0780b2fe9eddaa763774e190300beda8ae6dd833
Eric Ross <epr@rossmbox.com>
2023-08-03 20:37:37 -0700

    Added meals -- in-process

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,11 @@
+const MealsSummary = () => {
+    return <section>
+        <h2>Delicious Food, Delivered To You</h2>
+        <p>
+            Choose your favorite meal from our awesome menu.
+        </p>
+        <p>
+            All our meals are cooked in the best way.
+        </p>
+    </section>
+};
