81b814c110970a4de0b292f15fcd0c3f160f7142
Eric Ross <epr@rossmbox.com>
2023-09-20 19:50:40 -0700

    Move change to component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
 
 import "./App.css";
 
@@ -12,7 +13,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new</p>}
+      <DemoOutput show={showParagraph}></DemoOutput>
       <Button onClick={toggleParagraphHandler}>Show!</Button>
     </div>
   );
